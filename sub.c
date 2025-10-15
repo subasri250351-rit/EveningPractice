19d94fe5ba2d229529f5995ab9492bf737cde5a9
subasri250351-rit <subasri.250351@aids.ritchennai.edu.in>
2025-10-15 16:34:55 +0530

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,ans;
+printf("enter first number:");
+scanf("%d",&num1);
+printf("enter second number:");
+scanf("%d",&num2);
+ans=num1-num2;
+printf("ans=%d",ans);
+return 0;
+}
