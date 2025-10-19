98405b2acc3a19f158e608374283dd0d8ee3c39f
subasri250351-rit <subasri.250351@aids.ritchennai.edu.in>
2025-10-19 12:31:02 +0530

    Add files via upload

---
+++ c pro 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int num1,num2,sum;
+	printf("enter first number:\n");
+	scanf("d%",&num1);
+	printf("enter second number:\n");
+	scanf("%d",&num2);
+	sum=num1+num2;
+	printf("sum=%d \n",sum);
+	return 0;
+}
