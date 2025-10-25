e3cf57bd967dee7efd5e9965423f31bd1c11b8f9
subasri250351-rit <subasri.250351@aids.ritchennai.edu.in>
2025-10-25 17:52:36 +0530

    Add files via upload

---
+++ arith2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=10,b=4,c;
+	float d=3,e;
+	c=a/b;
+	printf("\n value of a/b is:%d"
+	,c);
+	e=a/d;
+	printf("\n value of a/d is:%f",e);
+}
