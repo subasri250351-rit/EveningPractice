e8ee9ef252559240c2030b23c2c8bb3f6d4d4f3e
subasri250351-rit <subasri.250351@aids.ritchennai.edu.in>
2025-11-05 18:06:33 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int num;
+	printf("enter a number\n");
+	scanf("%d",&num);
+	if(num%2==0)
+	 goto even;
+	else
+	 goto odd;
+	even:
+		printf("%d is even\n",num);
+		exit(0);
+	odd:
+		printf("%d is odd\n",num);
+}
